d58139334c852e408e822fb131ea894b81aa958d
Szymon <malpkamalpka58@gmail.com>
2020-07-08 17:56:14 +0200

    zmiana algorytmu na n1

---
+++ podmiana_liter.py
@@ -0,0 +1,13 @@
+lokalizacja = input("Lokalizacja pliku z danymi wejściowymi: ")
+plik = open(lokalizacja)
+inpx = plik.readlines()
+inp = inpx[0].strip()
+znaki1 = ["a", "e", "i", "o", "s"]
+znaki2 = ["4", "3", "1", "0", "5"]
+licznik = 0
+for u in range(len(inp)):
+        while licznik != len(znaki1):        
+                if inp[u] == znaki1[licznik]:
+                        inp = inp[:u] + znaki2[licznik] + inp[u + 1:]
+                licznik += 1
+print(inp)
