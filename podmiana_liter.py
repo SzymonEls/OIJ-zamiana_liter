1f72b16fe0457f2bcc209f838652efd1bb700a76
Szymon <malpkamalpka58@gmail.com>
2020-07-09 10:21:00 +0200

    poprawki

--- podmiana_liter.py
+++ podmiana_liter.py
@@ -1,4 +1,6 @@
 lokalizacja = input("Lokalizacja pliku z danymi wejściowymi: ")
+lokalizacja2 = input("Lokalizacja pliku z poprawnymi odpowiedziami: ")
+
 plik = open(lokalizacja)
 inpx = plik.readlines()
 inp = inpx[0].strip()
@@ -10,4 +12,12 @@ for u in range(len(inp)):
                 if inp[u] == znaki1[licznik]:
                         inp = inp[:u] + znaki2[licznik] + inp[u + 1:]
                 licznik += 1
-print(inp)
+        licznik = 0
+#print(inp)
+
+plik2 = open(lokalizacja2)
+odpx = plik2.readlines()
+odp = odpx[0].strip()
+#print(odp)
+if inp == odp:
+        print("Dobrze!!!!!!")
